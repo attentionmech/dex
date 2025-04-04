44e99c5a407217d1f7098370ca12ffd59a3996aa
attentionmech <attentionmech@gmail.com>
2025-04-05 02:38:11 +0530

    selection of individual blocks

--- loshdex/src/components/DexModelVisualiser.js
+++ loshdex/src/components/DexModelVisualiser.js
@@ -1,9 +1,8 @@
 import { StandardMaterial, MeshBuilder, Vector3, ActionManager, ExecuteCodeAction, DynamicTexture, Plane, Color3, Mesh } from "@babylonjs/core";
 
-import { GlowLayer } from "@babylonjs/core/Layers/glowLayer";
-
 import { CONFIG } from "../commons/Configs";
 import { RAINBOW_COLORS } from "../commons/Constants";
+import { TransformNode, Animation } from "@babylonjs/core";
 
 
 export class DexModelVisualizer {
@@ -11,6 +10,11 @@ export class DexModelVisualizer {
     this.scene = scene;
     this.uiComponents = uiComponents;
     this.layerColorAssignments = {};
+    this.modelRoot = new TransformNode("modelRoot", this.scene);
+    this.selectedDisk = null;
+
+    this.disks = [];
+
   }
 
   getCleanLayerName(originalName) {
@@ -20,16 +24,16 @@ export class DexModelVisualizer {
   createLabelPlane(text, scene) {
     const dynamicTexture = new DynamicTexture("labelTexture", { width: 1024, height: 128 }, scene, false);
     dynamicTexture.hasAlpha = true;
-  
+
     dynamicTexture.drawText(text, null, 80, "bold 100px Arial", "white", "black", true);
-  
+
     const labelMaterial = new StandardMaterial("labelMaterial", scene);
     labelMaterial.diffuseTexture = dynamicTexture;
     labelMaterial.emissiveColor = new Color3(1, 1, 1);
     labelMaterial.backFaceCulling = false;
     labelMaterial.alpha = 0.8; // Adjust alpha value for transparency
 
-  
+
     const plane = MeshBuilder.CreatePlane("labelPlane", { width: 200, height: 50 }, scene);
     plane.material = labelMaterial;
     plane.billboardMode = Mesh.BILLBOARDMODE_ALL;
@@ -38,10 +42,10 @@ export class DexModelVisualizer {
     // const glowLayer = new GlowLayer("glow", this.scene);
     // glowLayer.addExcludedMesh(plane);
 
-  
+
     return plane;
   }
-  
+
 
   assignLayerColor(layerName) {
     if (!this.layerColorAssignments[layerName]) {
@@ -61,7 +65,7 @@ export class DexModelVisualizer {
     const logMaxSize = Math.max(...layerSizes.map(Math.log));
     let currentYPosition = CONFIG.STARTING_Y_POSITION;
     let currentXPosition = 0;
-    const disks = [];
+    this.disks = [];
 
     layerData.forEach((layer, index) => {
       const layerCleanName = this.getCleanLayerName(layer.name);
@@ -116,15 +120,44 @@ export class DexModelVisualizer {
           this.uiComponents.panelText.text = "loshdex";
         })
       );
+      
+      disk.actionManager.registerAction(
+        new ExecuteCodeAction(ActionManager.OnPickTrigger, () => {
+          this.highlightDisk(disk, this.disks);
+        })
+      );
+      
 
-      disks.push(disk);
+      disk.parent = this.modelRoot;
+
+      this.disks.push(disk);
     });
+
+    this.scene.onPointerDown = (evt, pickInfo) => {
+      // Ignore if click hit a mesh and it's one of the disks
+      if (pickInfo.hit && this.selectedDisk && this.modelRoot.getChildren().includes(pickInfo.pickedMesh)) {
+        return; // Don't reset if clicking a disk
+      }
+    
+      // Else reset everything
+      this.selectedDisk = null;
+      this.disks.forEach(disk => {
+        const mat = disk.material;
+        if (mat) {
+          mat.alpha = 1;
+          mat.emissiveColor = mat.diffuseColor.scale(CONFIG.COLOR_EMISSIVE_MULTIPLIERS);
+        }
+      });
+    };
+    
+    
 
     const extent = CONFIG.MODEL_DIRECTION === 'horizontal' ? currentXPosition : currentYPosition;
     const target = CONFIG.MODEL_DIRECTION === 'horizontal'
       ? new Vector3(currentXPosition / 2, 0, 0)
       : new Vector3(0, currentYPosition / 2, 0);
 
+    const disks = this.disks;
     return { disks, target, extent };
   }
 
@@ -132,4 +165,57 @@ export class DexModelVisualizer {
     disks.forEach(disk => disk.dispose());
     return [];
   }
+
+  highlightDisk(selectedDisk, allDisks) {
+    this.selectedDisk = selectedDisk;
+  
+    allDisks.forEach(disk => {
+      const mat = disk.material;
+      if (!mat) return;
+  
+      if (disk === selectedDisk) {
+        mat.alpha = 1;
+        mat.emissiveColor = mat.diffuseColor.scale(CONFIG.COLOR_EMISSIVE_MULTIPLIERS);
+      } else {
+        mat.alpha = 0.2;
+        mat.emissiveColor = new Color3(0, 0, 0);
+      }
+    });
+  }
+  
+  
+  
+  // unused
+  focusOnDisk(disk) {
+    const targetPosition = disk.getAbsolutePosition().negate(); // Reverse it to bring to (0,0,0)
+  
+    // Only move along X or Y depending on model direction
+    const current = this.modelRoot.position.clone();
+    const target = current.clone();
+  
+    if (CONFIG.MODEL_DIRECTION === "horizontal") {
+      target.x = targetPosition.x;
+    } else {
+      target.y = targetPosition.y;
+    }
+  
+    const animation = new Animation(
+      "modelShift",
+      "position",
+      60,
+      Animation.ANIMATIONTYPE_VECTOR3,
+      Animation.ANIMATIONLOOPMODE_CONSTANT
+    );
+  
+    animation.setKeys([
+      { frame: 0, value: current },
+      { frame: 15, value: target }
+    ]);
+  
+    this.modelRoot.animations = [];
+    this.modelRoot.animations.push(animation);
+    this.scene.beginAnimation(this.modelRoot, 0, 30, false);
+  }
+  
+
 }
