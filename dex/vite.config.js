a248df8a945e4845453c78fcb51acfa88fa7f988
attentionmech <attentionmech@gmail.com>
2025-04-10 13:27:28 +0530

    change layout of main page

--- dex/vite.config.js
+++ dex/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/dex/", // important if you deploy under a subpath
+  base: "/dex/", // important if you deploy under a subpath
   build: {
     outDir: "dist", // default
   },
